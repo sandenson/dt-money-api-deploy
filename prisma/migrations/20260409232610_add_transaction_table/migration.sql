e3138be83a634a25a7710d37fe6a6a4c4e58857a
italocarlo06 <italocarlo06@gmail.com>
2026-04-30 20:26:03 -0300

    add: postgres db

--- prisma/migrations/20260409232610_add_transaction_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "transactions" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "price" REAL NOT NULL,
-    "category" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "data" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- src/shared/prisma.service.ts
+++ src/shared/prisma.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const adapter = new PrismaPg( { connectionString });
+    super( { adapter});
+  }
+}
+/*export class PrismaService extends PrismaClient {
+  constructor() {
     const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
     super({ adapter });
   }
-}
+}*/
