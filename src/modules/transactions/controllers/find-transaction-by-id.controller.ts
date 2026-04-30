e5ad77400cb5967c56b4563a84b4dc067a46a9ad
italocarlo06 <italocarlo06@gmail.com>
2026-04-30 17:41:49 -0300

    add: swagger annotations

--- src/modules/transactions/controllers/find-transaction-by-id.controller.ts
+++ src/modules/transactions/controllers/find-transaction-by-id.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Get, HttpStatus, Param, Res } from "@nestjs/common";
 import { FindTransactionByIdService } from "../services/find-transaction-by-id.service";
 import type { Response } from 'express'
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 
+@ApiTags('transactions')
 @Controller('transactions')
 export class FindTransactionByIdController {
 
     constructor(private readonly findTransactionById: FindTransactionByIdService){}
 
     @Get('/:id')
+    @ApiOperation({ summary: 'Buscar uma transação financeira por ID' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Transação encontrada com sucesso.' })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Transação não encontrada.' })
+    @ApiParam({ name: 'id', description: 'ID da transação a ser buscada', example: '123e4567-e89b-12d3-a456-426614174000' })
       async handle(@Param('id') id: string, @Res()res: Response) {
         const transaction = await this.findTransactionById.execute(id);
         return res.status(HttpStatus.OK).json(transaction);
--- src/modules/transactions/controllers/get-transactions.controller.ts
+++ src/modules/transactions/controllers/get-transactions.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Res, HttpStatus } from "@nestjs/common";
 import { GetTransactionsService } from "../services/get-transactions.service";
 import type { Response } from 'express';
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags('transactions')
 @Controller('transactions')
 export class GetTransactionsController {
     constructor( private readonly getTransactionsService: GetTransactionsService){
@@ -9,6 +11,8 @@ export class GetTransactionsController {
     }
 
     @Get('')
+    @ApiOperation({ summary: 'Listar todas as transações financeiras' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Lista de transações retornada com sucesso.' })
     async getTransactions(@Res() res: Response) {
     const transactions = await this.getTransactionsService.execute();
     return res.status(HttpStatus.OK).json(transactions);
