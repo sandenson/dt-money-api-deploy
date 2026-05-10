4f0fec7f8bcdecdf1833c945f6992671a8663b11
sandenson <lgabrielsandes@gmail.com>
2026-05-10 18:11:38 -0300

    fix: Set return types for the methods of the ITransactionRepository abstract class.

--- src/modules/transactions/infra/repositories/transaction.repository.abstract.ts
+++ src/modules/transactions/infra/repositories/transaction.repository.abstract.ts
@@ -1,10 +1,11 @@
+import { Transaction } from 'generated/prisma/client';
 import { CreateTransactionDTO } from '../../dto/create-transaction.dto';
 import { UpdateTransactionDTO } from '../../dto/update-transaction.dto';
 
 export abstract class ITransactionRepository {
-  abstract create(data: CreateTransactionDTO);
-  abstract findAll();
-  abstract findById(id: string);
-  abstract delete(id: string);
-  abstract update(id: string, data: UpdateTransactionDTO);
+  abstract create(data: CreateTransactionDTO): Promise<Transaction>;
+  abstract findAll(): Promise<Transaction[]>;
+  abstract findById(id: string): Promise<Transaction>;
+  abstract delete(id: string): Promise<void>;
+  abstract update(id: string, data: UpdateTransactionDTO): Promise<Transaction>;
 }
